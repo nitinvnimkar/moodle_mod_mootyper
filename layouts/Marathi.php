7d4b31f36993ed49b09e45df5b04ab63703c05ce
Nitin Nimkar <nvncom@yahoo.com>
2013-02-12 04:48:17 +0530

    Marathi PHP with BOM
    Both Shift and Non Shift charecgers added

---
+++ layouts/Marathi.php
@@ -0,0 +1,68 @@
+<div id="innerTipkovnica" style="margin: 0px auto;display: inline-block;"><br>
+<span id="jkeytildo" class="normal"> </span>
+<span id="jkey1" class="normal">१</span>
+<span id="jkey2" class="normal">२</span>
+<span id="jkey3" class="normal">३</span>
+<span id="jkey4" class="normal">४</span>
+<span id="jkey5" class="normal">५</span>
+<span id="jkey6" class="normal">६</span>
+<span id="jkey7" class="normal">७</span>
+<span id="jkey8" class="normal">८</span>
+<span id="jkey9" class="normal">९</span>
+<span id="jkey0" class="normal">०</span>
+<span id="jkeypomislaj" class="normal">-</span>
+<!--span id="jkeyvprasaj" class="normal">?</span-->
+<!--span id="jkeyplus" class="normal">+</span-->
+<span id="jkeyequals" class="normal">ृ</span>
+<span id="jkeybackspace" class="normal" style="border-right-style: solid;">Backspace</span><br>
+<span id="jkeytab" class="normal" style="width: 50px;">Tab</span>
+<span id="jkeyq" class="normal">ौऔ</span>
+<span id="jkeyw" class="normal">ैऐ</span>
+<span id="jkeye" class="normal">ाआ</span>
+<span id="jkeyr" class="normal">ीई</span>
+<span id="jkeyt" class="normal">ूऊ</span>
+<span id="jkeyy" class="normal">बभ</span>
+<span id="jkeyu" class="normal">हङ</span>
+<span id="jkeyi" class="normal">गघ</span>
+<span id="jkeyo" class="normal">दध</span>
+<span id="jkeyp" class="normal">जझ</span>
+<span id="jkeyoglokl" class="normal">डढ</span>
+<span id="jkeyoglzak" class="normal" style="border-right-style: solid;">़</span>
+<br>
+<span id="jkeycaps" class="normal" style="width: 60px;">C.lock</span>
+<span id="jkeya" class="finger4">ोओ</span>
+<span id="jkeys" class="finger3">ेए</span>
+<span id="jkeyd" class="finger2">्अ</span>
+<span id="jkeyf" class="finger1">िइ</span>
+<span id="jkeyg" class="normal">ुउ</span>
+<span id="jkeyh" class="normal">पफ</span>
+<span id="jkeyj" class="finger1">रऱ</span>
+<span id="jkeyk" class="finger2">कख</span>
+<span id="jkeyl" class="finger3">तथ</span>
+<span id="jkeypodpicje" class="finger4">चछ</span>
+<span id="jkeycrtica" class="normal">टठ</span>
+<span id="jkeybackslash" class="normal">ॉऑ</span>
+<span id="jkeyenter" class="normal" style="border-right-style: solid;">Enter</span>
+<br>
+<span id="jkeyshiftl" class="normal" style="width: 50px;">Shift</span>
+<span id="jkeyckck" class="normal">&lt; &gt;</span>
+<span id="jkeyz" class="normal"></span>
+<span id="jkeyx" class="normal">ंँ</span>
+<span id="jkeyc" class="normal">मण</span>
+<span id="jkeyv" class="normal">न</span>
+<span id="jkeyb" class="normal">व</span>
+<span id="jkeyn" class="normal">लळ</span>
+<span id="jkeym" class="normal">सश</span>
+<span id="jkeyvejica" class="normal">,</span>
+<span id="jkeypika" class="normal">.</span>
+<span id="jkeyslash" class="normal">यय़</span>
+<span id="jkeyshiftd" class="normal" style="width: 75px; border-right-style: solid;">Shift</span>
+<br>
+<span id="jkeyctrll" class="normal" style="width: 40px;">Ctrl</span>
+<span id="jkeyfn" class="normal">Fn</span>
+<span id="jkeyalt" class="normal" style="width: 40px;">Alt</span>
+<span id="jkeyspace" class="normal" style="width: 250px;">Space</span>
+<span id="jkeyaltgr" class="normal" style="width: 45px;">Alt gr</span>
+<!--span id="jempty" class="normal" style="width: 30px;">&nbsp;</span-->
+<span id="jkeyctrlr" class="normal" style="width: 60px; border-right-style: solid;">Ctrl</span><br>
+</div>
